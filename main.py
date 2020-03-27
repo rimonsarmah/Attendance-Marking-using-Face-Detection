5b43637f74dd74d53bb6b525405e6c60b15e46ca
Rimon Sarmah <38400175+rimonsarmah@users.noreply.github.com>
2020-03-28 02:07:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-import PrettyTable
-import sqllite3
+import os
+import sqlite3
+import getpass
+import face_recognition
+import cv2
+from prettytable import PrettyTable
+from datetime import date
+import matplotlib.pyplot as plt
+import numpy as np
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+#cursor.execute('CREATE TABLE LOGIN_TABLE(u_id varchar(30), pass varchar(20), u_type varchar(20));')
+#cursor.execute('INSERT INTO LOGIN_TABLE VALUES("admin","abcd","admin");')
+#conn.commit()
+login = -1
+while login!=1:
+    username = input('Enter username: ')
+    print('Enter password: ')
+    password = getpass.getpass()
+    cursor.execute('SELECT * FROM LOGIN_TABLE WHERE u_id=?;',(username,))
+    for x in cursor:
+        global pass_valid
+        pass_valid = x[1]
+        global user_type
+        user_type = x[2]
+    if pass_valid == password:
+        login = 1
+    else:
+        print("I'm afraid! You have entered a wrong password. Please try again.")
