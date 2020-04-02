9f2ca4a3a475dbf1519b66bec8d6624f845da7c7
Rimon Sarmah <38400175+rimonsarmah@users.noreply.github.com>
2020-04-02 22:29:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ if user_type == 'admin':
          ------------------------------------------------------
                              Welcome Admin!
 
-                    Enter 1 : To View List of Faculty
-                    Enter 2 : To View List of Students
-                    Enter 3 : To Add New Student
-                    Enter 4 : To Add New Faculty
+                    Enter 1 : To Add New Student
+                    Enter 2 : To Add New Faculty
+                    Enter 3 : To View List of Faculty
+                    Enter 4 : To View List of Students
                     Enter 5 : To Search Student
                     Enter 6 : To Search Faculty
                     Enter 7 : To Delete Student
@@ -60,6 +60,126 @@ if user_type == 'admin':
         speak.say("Choose an option please.")
         speak.runAndWait()
         choice = int(input('Enter your choice: '))
+        if choice == 1:
+            print('Add New Student')
+            s_section = input("Enter student's section: ")
+            s_name = input('Enter student name: ')
+            s_fname = input("Enter Father's name: ")
+            s_mname = input("Enter Mother's name: ")
+            s_email = input("Enter email id: ")
+            s_phone = input("Enter student's phone no: ")
+            s_pphone = input("Enter parent's phone no: ")
+            s_address = input("Enter address: ")
+            #cursor.execute("CREATE TABLE STUDENT(s_id integer PRIMARY KEY AUTOINCREMENT,s_section varchar(20), s_name varchar(30),s_fname varchar(30),s_mname varchar(30),s_email varchar(30),s_phone varchar(20),s_pphone varchar(30),s_address varchar(30));")
+            cursor.execute("INSERT INTO STUDENT(s_section,s_name,s_fname,s_mname,s_email,s_phone,s_pphone,s_address) VALUES(?,?,?,?,?,?,?,?);",(s_section,s_name,s_fname,s_mname,s_email,s_phone,s_pphone,s_address))
+            conn.commit()
+            print('Added to STUDENT Table')
+            print('Data Successfully Entered')
+            dummy = input('Press any key to continue...........')
+        elif choice == 2:
+            print('Add New Faculty')
+            f_name = input('Enter Faculty name: ')
+            f_subject = input("Enter subject: ")
+            f_email = input("Enter email id: ")
+            f_phone = input("Enter phone no: ")
+            f_address = input("Enter address: ")
+            f_pw = 'abcd'
+            ac_type = 'faculty'
+            #cursor.execute("CREATE TABLE FACULTY_DATA(f_id integer PRIMARY KEY AUTOINCREMENT,f_name varchar(30), f_subject varchar(30),f_email varchar(30),f_phone varchar(30),f_address varchar(30),f_pass varchar(30));")
+            cursor.execute("INSERT INTO FACULTY_DATA(f_name,f_subject,f_email,f_phone,f_address,f_pass) VALUES(?,?,?,?,?,?);",(f_name,f_subject,f_email,f_phone,f_address,f_pw))
+            conn.commit()
+            cursor.execute("INSERT INTO LOGIN_TABLE VALUES(?,?,?);",(f_email,f_pw,ac_type));
+            conn.commit()
+            print('Data Successfully Entered')
+            dummy = input('Press any key to continue...........')
+        elif choice == 3:
+            print('List of Faculties: ')
+            x = PrettyTable()
+            cursor.execute("SELECT * from FACULTY_DATA")
+            x.field_names = ["ID", "Name", "Subject", "Email", "Phone", "Address", "Password"]
+            for row in cursor:
+                x.add_row([row[0],row[1],row[2],row[3],row[4],row[5],row[6]]);
+            print(x)
+            dummy = input('Press any key to continue...........')
+        elif choice == 4:
+            x = PrettyTable()
+            cursor.execute("SELECT * from STUDENT")
+            x.field_names = ["ID", "Section", "Name", "Father's Name", "Mother's Name", "Email", "Phone No", "Parent's Phone", "Address"]
+            for row in cursor:
+                x.add_row([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]]);
+            print(x)
+            dummy = input('Press any key to continue...........')
+        elif choice == 5:
+            print("1)Search by ID")
+            print("2)Search by Name:")
+            choice = int(input('Enter 1 or 2......'))
+            if choice == 1:
+                key = input('Enter ID: ')
+                x = PrettyTable()
+                cursor.execute("SELECT * from STUDENT_DATA WHERE s_id = ?;", key)
+                x.field_names = ["ID", "Section", "Name", "Father's Name", "Mother's Name", "Email", "Phone No", "Parent's Phone", "Address"]
+                for row in cursor:
+                    x.add_row([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]]);
+                print(x)
+                dummy = input('Press any key to continue...........')
+            if choice == 2:
+                key = input('Enter Name: ')
+                key = key+'%'
+                x = PrettyTable()
+                cursor.execute("SELECT * from STUDENT_DATA WHERE s_name LIKE ?;", (key,))
+                x.field_names = ["ID", "Section", "Name", "Father's Name", "Mother's Name", "Email", "Phone No", "Parent's Phone", "Address"]
+                for row in cursor:
+                    x.add_row([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]]);
+                print(x)
+                dummy = input('Press any key to continue...........')
+        elif choice == 6:
+            print("Search Faculty")
+            key = input('Enter Name: ')
+            x = PrettyTable()
+            cursor.execute("SELECT * from FACULTY_DATA where f_name LIKE ?;", key)
+            x.field_names = ["ID", "Name", "Subject", "Email", "Phone", "Address", "Password"]
+            for row in cursor:
+                x.add_row([row[0],row[1],row[2],row[3],row[4],row[5],row[6]]);
+            print(x)
+            dummy = input('Press any key to continue...........')
+        elif choice == 7:
+            print('Delete Student: ')
+            key = input('Enter ID: ')
+            cursor.execute("DELETE from STUDENT WHERE s_id = ?;", (key,))
+            conn.commit()
+            print('Success')
+            dummy = input('Press any key to continue...........')
+        elif choice == 8:
+            print('Delete Faculty: ')
+            key = input('Enter email ID: ')
+            cursor.execute("DELETE from FACULTY_DATA WHERE f_email = ?;", (key,))
+            print('Deleted from FACULTY_DATA')
+            conn.commit()
+            cursor.execute("DELETE from LOGIN_TABLE WHERE u_id = ?;", (key,))
+            print('Deleted from LOGIN_TABLE')
+            conn.commit()
+            dummy = input('Press any key to continue...........')
+        elif choice == 9:
+            success = -1
+            while success!=1:
+                print('Enter password: ')
+                password = getpass.getpass()
+                cursor.execute('SELECT * FROM LOGIN_TABLE WHERE u_id=?;',(username,))
+                for x in cursor:
+                    pass_val = x[1]
+                    if pass_val == password:
+                        newp = input('Please enter new password: ')
+                        cursor.execute("UPDATE LOGIN_TABLE set pass=? where u_id=?;",(newp,username))
+                        conn.commit()
+                        success = 1
+                        print('Password Changed')
+                        login = 0
+                    else:
+                        print("I'm afraid! You have entered a wrong password. Please try again.")
+        elif choice == 10:
+            login = 0
+            print('You are successfully logged out')
+        
 elif user_type == 'faculty':
     speak.say("Hello Faculty! Welcome to Attendance Marking System Using Face Detection. Hope you'll find it interesting.")
     speak.runAndWait()
